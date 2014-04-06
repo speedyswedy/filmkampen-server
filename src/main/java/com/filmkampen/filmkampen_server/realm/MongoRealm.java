692c697648d6323ec3292bdecd607bebe377bc22
Pelle <speedyswedy@hotmail.com>
2014-04-06 15:52:53 +0200

    Changed MongoRealm

--- src/main/java/com/filmkampen/filmkampen_server/realm/MongoRealm.java
+++ src/main/java/com/filmkampen/filmkampen_server/realm/MongoRealm.java
@@ -13,28 +13,28 @@ import org.springframework.stereotype.Service;
 import com.filmkampen.filmkampen_server.service.UserService;
 
 @Service
-public class MongoRealm extends RealmBase {
+public class MongoRealm {//extends RealmBase {
     
-    @Resource
-    private UserService userService;
-
-    @Override
-    protected String getName() {
-        return this.getClass().getSimpleName();
-    }
-
-    @Override
-    protected String getPassword(final String username) {
-        System.out.println("getPassword:" + userService);
-        return userService.getPasswordByUsername(username);
-    }
-
-    @Override
-    protected Principal getPrincipal(final String username) {
-        String name = "ADMIN";
-        final List<String> roles = new ArrayList<String>();
-        roles.add(name);
-        String password = getPassword(username);
-        return new GenericPrincipal(username, password, roles);
-    }
+//    @Resource
+//    private UserService userService;
+//
+//    @Override
+//    protected String getName() {
+//        return this.getClass().getSimpleName();
+//    }
+//
+//    @Override
+//    protected String getPassword(final String username) {
+//        System.out.println("getPassword:" + userService);
+//        return userService.getPasswordByUsername(username);
+//    }
+//
+//    @Override
+//    protected Principal getPrincipal(final String username) {
+//        String name = "ADMIN";
+//        final List<String> roles = new ArrayList<String>();
+//        roles.add(name);
+//        String password = getPassword(username);
+//        return new GenericPrincipal(username, password, roles);
+//    }
 }
