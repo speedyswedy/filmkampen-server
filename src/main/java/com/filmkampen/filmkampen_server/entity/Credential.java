e8d28d2da91bbb4fbae0812004ed78dec92fbc2b
Pelle <speedyswedy@hotmail.com>
2014-04-08 20:39:06 +0200

    added LoginResource

---
+++ src/main/java/com/filmkampen/filmkampen_server/entity/Credential.java
@@ -0,0 +1,27 @@
+package com.filmkampen.filmkampen_server.entity;
+
+public class Credential {
+    private String userName;
+    private String password;
+    private String token;
+    
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
+}
---
+++ src/main/java/com/filmkampen/filmkampen_server/resource/LoginResource.java
@@ -0,0 +1,44 @@
+package com.filmkampen.filmkampen_server.resource;
+
+import javax.annotation.Resource;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+
+import com.filmkampen.filmkampen_server.entity.Credential;
+import com.filmkampen.filmkampen_server.entity.User;
+import com.filmkampen.filmkampen_server.manager.PersistenceManager;
+import com.filmkampen.filmkampen_server.service.UserService;
+
+@Path("/login")
+@Component
+@Produces("application/json")
+public class LoginResource {
+    
+    private Log LOG = LogFactory.getLog(LoginResource.class);
+
+    @Resource
+    private UserService userService;
+
+    @POST
+    public Credential login(Credential credential) {
+        credential.setToken(null);
+        User user = userService.findByUsername(credential.getUserName());
+        if (user != null) {
+            String password = user.getPassword();
+            if (password != null && password.equals(credential.getPassword())) {
+                String token = "123456qwert";
+                credential.setToken(token);
+                LOG.info("Token is returned.");
+            }
+        }
+        return credential;
+    }
+
+    
+    
+}
