cc04910ea9839eace114bb232744ff548d2f2549
Pelle <speedyswedy@hotmail.com>
2014-04-10 01:21:09 +0200

    added headers fix2

--- src/main/java/com/filmkampen/filmkampen_server/filter/CorsFilter.java
+++ src/main/java/com/filmkampen/filmkampen_server/filter/CorsFilter.java
@@ -36,7 +36,7 @@ public class CorsFilter implements Filter {
     private void addHeadersFor200Response(HttpServletResponse response) {
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Credentials", "true");
-        response.addHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, X-XSRF-TOKEN, X-Access-Token");
+        response.addHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, X-XSRF-TOKEN, X-Access-Token, Access-Control-Allow-Origin");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
     }
 }
