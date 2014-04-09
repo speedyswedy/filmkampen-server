4c44b6d54c44cbc57b535cc852c8f5dfff9ff617
Pelle <speedyswedy@hotmail.com>
2014-04-09 23:07:52 +0200

    added spring security

--- src/main/java/com/filmkampen/filmkampen_server/service/UserService.java
+++ src/main/java/com/filmkampen/filmkampen_server/service/UserService.java
@@ -1,11 +1,19 @@
 package com.filmkampen.filmkampen_server.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import com.filmkampen.filmkampen_server.entity.User;
 
 @Component
-public class UserService extends Service<User> {
+public class UserService extends Service<User> implements UserDetailsService {
     
     public User findByUsername(String username) {
         return (User) em.createQuery("Select u from User u where u.userName = '" + username + "'").getResultList().get(0);
@@ -14,5 +22,17 @@ public class UserService extends Service<User> {
     public String getPasswordByUsername(String username) {
         User user =  (User) em.createQuery("Select u from User u where u.userName = '" + username + "'").getResultList().get(0);
         return user.getPassword();
-    } 
+    }
+   
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("Getting access details from employee dao !!");
+        
+        User user = findByUsername(username);
+        
+        List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>(Arrays.asList(new SimpleGrantedAuthority[]{ new SimpleGrantedAuthority("ROLE_USER") }));
+        
+        UserDetails userDetail = new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), true, true, true, true, authorities);
+        return userDetail;
+    }
 }
