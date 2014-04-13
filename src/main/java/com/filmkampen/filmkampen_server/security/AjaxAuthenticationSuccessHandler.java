ee3f78d95da56664e600c007d4ccf6a927c4cebc
Pelle <speedyswedy@hotmail.com>
2014-04-13 11:08:35 +0200

    authentication change11

--- src/main/java/com/filmkampen/filmkampen_server/security/AjaxAuthenticationSuccessHandler.java
+++ src/main/java/com/filmkampen/filmkampen_server/security/AjaxAuthenticationSuccessHandler.java
@@ -6,20 +6,23 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 public class AjaxAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-public void onAuthenticationSuccess(HttpServletRequest request,
-    HttpServletResponse response, Authentication auth)
-    throws IOException, ServletException {
-if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
-    response.getWriter().print(
-            "{success:true, targetUrl : \'"
-                    + this.getTargetUrlParameter() + "\'}");
-    response.getWriter().flush();
-} else {
-    super.onAuthenticationSuccess(request, response, auth);
+    private Log LOG = LogFactory.getLog(CorsAwareAuthenticationFilter.class);
+
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth) throws IOException,
+            ServletException {
+        LOG.info("####HERE XMLHTTPREquest");
+        if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+            response.getWriter().print("{success:true, targetUrl : \'" + this.getTargetUrlParameter() + "\'}");
+            response.getWriter().flush();
+        } else {
+            super.onAuthenticationSuccess(request, response, auth);
+        }
+    }
 }
-}}
