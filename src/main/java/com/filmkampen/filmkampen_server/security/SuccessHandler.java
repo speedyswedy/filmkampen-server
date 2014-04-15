419b8250abcb9ff627c02dd7774934bd93cdc8cd
Pelle <speedyswedy@hotmail.com>
2014-04-15 20:43:27 +0200

    authentication change36

--- src/main/java/com/filmkampen/filmkampen_server/security/SuccessHandler.java
+++ src/main/java/com/filmkampen/filmkampen_server/security/SuccessHandler.java
@@ -6,12 +6,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 public class SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
   
+    private Log LOG = LogFactory.getLog(SuccessHandler.class);
+
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth) throws IOException, ServletException {
+        LOG.info("onAuthenticationSuccess");
         if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
             response.getWriter().print(
                     "{success:true, targetUrl : \'"
