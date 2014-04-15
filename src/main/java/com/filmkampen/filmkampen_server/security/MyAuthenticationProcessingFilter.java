e3483322efbd2bb3076a655c40c4c6eff5e3634a
Pelle <speedyswedy@hotmail.com>
2014-04-15 22:22:51 +0200

    authentication change48

--- src/main/java/com/filmkampen/filmkampen_server/security/MyAuthenticationProcessingFilter.java
+++ src/main/java/com/filmkampen/filmkampen_server/security/MyAuthenticationProcessingFilter.java
@@ -7,17 +7,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
  
 public class MyAuthenticationProcessingFilter extends UsernamePasswordAuthenticationFilter {
  
+    private Log LOG = LogFactory.getLog(MyAuthenticationProcessingFilter.class);
+    
     protected void onSuccessfulAuthentication(HttpServletRequest request,
             HttpServletResponse response, Authentication authResult)
     throws IOException {
-       
- 
+        LOG.info("###### onSuccessfulAuth");
         response.getWriter().print(
                 "{success:true, targetUrl : \'"
                         + this.getFilterProcessesUrl() + "\'}");
@@ -29,6 +32,7 @@ public class MyAuthenticationProcessingFilter extends UsernamePasswordAuthentica
             HttpServletResponse response, AuthenticationException failed )
     throws IOException {
  
+        LOG.info("###### onUnsuccessfulAuth");
         HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(response);
  
         Writer out = responseWrapper.getWriter();
