afd8437f99848f80c7695251f30ce95d36581447
Pelle <speedyswedy@hotmail.com>
2014-04-15 20:52:04 +0200

    authentication change38

--- src/main/java/com/filmkampen/filmkampen_server/security/PlainTextBasicAuthenticationEntryPoint.java
+++ src/main/java/com/filmkampen/filmkampen_server/security/PlainTextBasicAuthenticationEntryPoint.java
@@ -20,13 +20,7 @@ public class PlainTextBasicAuthenticationEntryPoint extends
         public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
           LOG.info("############Plain commence");
           response.addHeader("Access-Control-Allow-Origin", "*");
-          //response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
-          //response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//          PrintWriter writer = response.getWriter();
-//          writer.println("HTTP Status " + HttpServletResponse.SC_UNAUTHORIZED + " - " + authException.getMessage());
           response.addHeader("Access-Control-Allow-Credentials", "true");
-          //response.addHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, X-XSRF-TOKEN, X-Access-Token, Access-Control-Allow-Origin, Overrite, Destination, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, accept");
-          //response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, PROPFIND, PROPPATCH, COPY, MOVE, MKCOL, LOCK, GETLIB, VERSION-ONCTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD");
           response.addHeader("Access-Control-Allow-Headers", "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
           response.addHeader("Access-Control-Allow-Methods", "GET,POST,HEAD,PUT");
           response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
--- src/main/java/com/filmkampen/filmkampen_server/security/SuccessHandler.java
+++ src/main/java/com/filmkampen/filmkampen_server/security/SuccessHandler.java
@@ -16,7 +16,7 @@ public class SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private Log LOG = LogFactory.getLog(SuccessHandler.class);
 
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth) throws IOException, ServletException {
-        LOG.info("onAuthenticationSuccess");
+        LOG.info("#########onAuthenticationSuccess");
         if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
             response.getWriter().print(
                     "{success:true, targetUrl : \'"
