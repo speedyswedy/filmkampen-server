1282183dd3e4b6fdcd8b86522a934e6a5de36791
Pelle <speedyswedy@hotmail.com>
2014-04-17 21:06:01 +0200

    changed security file5

--- src/main/java/com/filmkampen/filmkampen_server/security/VozisBasicAuthenticationEntryPoint.java
+++ src/main/java/com/filmkampen/filmkampen_server/security/VozisBasicAuthenticationEntryPoint.java
@@ -7,20 +7,23 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 public class VozisBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
-@Override
-public void commence(HttpServletRequest request, HttpServletResponse response,
-AuthenticationException authException) throws IOException, ServletException {
-      response.addHeader("WWW-Authenticate", "Basic realm=\"" +
-         getRealmName() + "\"");
-      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-      PrintWriter writer = response.getWriter();
-      writer.println("HTTP Status " + HttpServletResponse.SC_UNAUTHORIZED +
-         " - " + authException.getMessage());
-   }
+    private Log LOG = LogFactory.getLog(VozisBasicAuthenticationEntryPoint.class);
+    
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        
+        LOG.info("commence NEW");
+        response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status " + HttpServletResponse.SC_UNAUTHORIZED + " - " + authException.getMessage());
+    }
 }
-
