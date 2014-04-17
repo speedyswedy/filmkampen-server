1cf66487c2a827961010aa7c1af83c9f63e2e39a
Pelle <speedyswedy@hotmail.com>
2014-04-17 22:43:26 +0200

    changed security fi1

--- src/main/java/com/filmkampen/filmkampen_server/security/MyBasic.java
+++ src/main/java/com/filmkampen/filmkampen_server/security/MyBasic.java
@@ -1,6 +1,7 @@
 package com.filmkampen.filmkampen_server.security;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,10 +19,13 @@ public class MyBasic extends BasicAuthenticationFilter {
     protected void onSuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, Authentication authResult)
             throws IOException {
         LOG.info("###### onSuccessfulAuthentication");
-        response.getWriter().print(
-                "{success:true, targetUrl : \'"
-                        + this.getServletContext().getRealPath("/home") + "\'}");
-        response.getWriter().flush();
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+        response.addHeader("Access-Control-Allow-Methods", "GET,POST,HEAD,PUT");
+        response.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter writer = response.getWriter();
+        writer.println("OK");
     }
 
 }
