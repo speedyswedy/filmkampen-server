04ca484684e365204ffa0371e71ec40b8a620e28
Pelle <speedyswedy@hotmail.com>
2014-04-17 21:00:08 +0200

    changed security file4

---
+++ src/main/java/com/filmkampen/filmkampen_server/security/VozisBasicAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.filmkampen.filmkampen_server.security;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+public class VozisBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+@Override
+public void commence(HttpServletRequest request, HttpServletResponse response,
+AuthenticationException authException) throws IOException, ServletException {
+      response.addHeader("WWW-Authenticate", "Basic realm=\"" +
+         getRealmName() + "\"");
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      PrintWriter writer = response.getWriter();
+      writer.println("HTTP Status " + HttpServletResponse.SC_UNAUTHORIZED +
+         " - " + authException.getMessage());
+   }
+}
+
