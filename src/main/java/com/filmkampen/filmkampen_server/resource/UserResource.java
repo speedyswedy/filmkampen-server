b47dc52eb6cb576dccde5f4e6518f5588d123a81
Pelle <speedyswedy@hotmail.com>
2014-04-21 17:43:26 +0200

    consumes JSON

--- src/main/java/com/filmkampen/filmkampen_server/resource/UserResource.java
+++ src/main/java/com/filmkampen/filmkampen_server/resource/UserResource.java
@@ -3,12 +3,14 @@ package com.filmkampen.filmkampen_server.resource;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,7 +23,8 @@ import com.filmkampen.filmkampen_server.service.UserService;
 
 @Path("/user")
 @Component
-@Produces("application/json")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)   
 public class UserResource {
     
     private Log LOG = LogFactory.getLog(UserResource.class);
