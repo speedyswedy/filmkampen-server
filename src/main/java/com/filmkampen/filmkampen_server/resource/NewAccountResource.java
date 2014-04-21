c5e3cbaa74c367df268d9d646b794b3e8aa66cd2
Pelle <speedyswedy@hotmail.com>
2014-04-21 18:16:53 +0200

    added a maven plugin4

--- src/main/java/com/filmkampen/filmkampen_server/resource/NewAccountResource.java
+++ src/main/java/com/filmkampen/filmkampen_server/resource/NewAccountResource.java
@@ -1,9 +1,11 @@
 package com.filmkampen.filmkampen_server.resource;
 
 import javax.annotation.Resource;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,7 +16,8 @@ import com.filmkampen.filmkampen_server.service.UserService;
 
 @Path("/newAccount")
 @Component
-@Produces("application/json")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)   
 public class NewAccountResource {
     
     private Log LOG = LogFactory.getLog(NewAccountResource.class);
